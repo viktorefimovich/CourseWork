9974fb16b7573289f363ac0c039cdc4a856e6b46
viktorefimovich <viktorefimovich@bk.ru>
2024-08-06 16:59:55 +0700

    add logger in investment_bank in services.py

--- src/services.py
+++ src/services.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 from pathlib import Path
 from typing import Any, Dict, List
@@ -6,16 +7,25 @@ from utils import get_transactions_read_excel
 
 ROOTPATH = Path(__file__).resolve().parent.parent
 
+logger = logging.getLogger("services")
+file_handler = logging.FileHandler(Path(ROOTPATH, "logs/services.log"), "w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
 
 def investment_bank(filtered_month: str, transactions_list: List[Dict[str, Any]], limit: int) -> float:
     """Функция возвращает сумму, которую удалось бы отложить в Инвесткопилку за введенный месяц"""
 
     total_amount: float = 0
+    logger.info("Проверяется дата каждой транзакции на вхождение в введенный месяц")
     for transaction in transactions_list:
         transaction_date = datetime.strptime(str(transaction.get("Дата операции")), "%d.%m.%Y %H:%M:%S")
         if transaction_date.strftime("%Y-%m") == filtered_month:
             if float(str(transaction.get("Сумма операции"))) < 0:
                 total_amount += limit - float(str(transaction.get("Сумма операции"))) % limit
+    logger.info("Возвращается сумма Инвесткопилки")
     return total_amount
 
 
